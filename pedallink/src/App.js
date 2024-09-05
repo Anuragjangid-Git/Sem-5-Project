dca52f308b786b3a9fef77a17e9acb493cc7e81c
[Anurag jangid] <[Anuragjangid-Git]>
2024-09-05 19:46:43 +0530

    commit-2

--- pedallink/src/App.js
+++ pedallink/src/App.js
@@ -1,16 +1,19 @@
 
 import './App.css';
-import Header from './components/common/header/Header';
-import HomePage from './components/pages/home/HomePage';
-import Login from './components/pages/register&login/Login';
+import Register from './components/pages/register&login/Register';
+// import Header from './components/common/header/Header';
+// import HomePage from './components/pages/home/HomePage';
+// import Login from './components/pages/register&login/Login';
 
 function App() {
   return (
     <div>
       
       {/* <Header/>
-      <HomePage/> */}
+      <HomePage/> 
      <Login/>
+     */}
+     <Register/>
     </div>
   );
 }
